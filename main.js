07a2e2f0a329faa12f54f1156731ffa16187a000
Jasonltc <jasonltc_14@hotmail.com>
2023-04-12 16:52:01 +0400

    game

--- main.js
+++ main.js
@@ -1,62 +1,79 @@
-// 1. Create two variables, firstCard and secondCard. 
-let firstCard,
-    secondCard,
-    sum,
-    hasBlackJack,
-    isAlive,
-    message,
-    startGameBtn,
-    messageEl,
-    playerCards,
-    drawACardBtn,
-    sumEl;
- 
-// assign value  
-firstCard = ''
-secondCard = ''
-hasBlackJack = false;
-isAlive = true;
-message = '';
-messageEl = document.querySelector(' #message-el' );
-sumEl = document.querySelector('.card-sum');
-playerCards = document.querySelector( '.player-cards' );
-drawACardBtn = document.querySelector(' .new-card-btn' );
-startGameBtn = document.querySelector( ' .start-btn' );
+// create variable and assign value;   
+let firstCard = randomNumber();
+let secondCard = randomNumber();
+let sumOfCards = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = '';
+let messageEl = document.querySelector(' #message-el' );
+let sumEl = document.querySelector('.card-sum');
+let playerCards = document.querySelector( '.player-cards' );
+let drawACardBtn = document.querySelector(' .new-card-btn' );
+let startGameBtn = document.querySelector( ' .start-btn' );
+let restartGameBtn = document.querySelector( ' .restart-btn' );
+let cards = [firstCard, secondCard];
 
-drawACardBtn.style.display = 'none';
+// Game at the begin and restart; 
+function initialGame() {
+  sumEl.textContent = '';
+  playerCards.textContent = '';  
+  drawACardBtn.style.display = 'none';
+  restartGameBtn.style.display = 'none';
+}
 
-// 2 Game start function
-function startGame() {
-     // Set their values to a random number between 2-11
-    firstCard = Math.floor((Math.random() * 10 )+ 2); 
-    secondCard = Math.floor( ( Math.random() * 10 ) + 2 );
-    sum = firstCard + secondCard;
+// create a random number between 2 and 11; 
+function randomNumber() {
+    let randomNumber = Math.floor( ( Math.random() * 10 ) + 2 );
+    return randomNumber;
+};
 
-    sumEl.textContent = sum;
-    playerCards.textContent = `${firstCard} ${secondCard}`;
-
-    if (sum <= 20) {
+// check the cards 
+function checkCards() {
+  if (sumOfCards <= 20) {
         message = "Do you want to draw a new card?";
         drawACardBtn.style.display = 'block';
         startGameBtn.style.display = 'none';
-    } else if ( sum === 21 ) {
+    } else if ( sumOfCards === 21 ) {
         hasBlackJack = true;
         message = "Wohoo! You've got Blackjack!";
     } else {
         isAlive = false;
-        message = "You're out of the game!";
+      message = "You're out of the game!";
+      drawACardBtn.style.display = 'none';
+      restartGameBtn.style.display = 'block';
     }
-
-    messageEl.textContent = message;
+     messageEl.textContent = message;
 }
 
+//function display cards and sum
+function displayCards() {
+    for ( let i = 0; i < cards.length; i++ ) {
+        playerCards.textContent += cards[ i ] + ' ';
+        sumEl.textContent = sumOfCards;
+    } 
+}
+
+// function draw another cards
 function drawACard() {
-    let thirdCard = firstCard = Math.floor( ( Math.random() * 10 ) + 2 ); 
-    sum = + thirdCard; 
-    startGame();
+    playerCards.textContent = " ";
+    let anotherCard = randomNumber();
+    cards.push( anotherCard );
+    sumOfCards += anotherCard;
+    displayCards();
+    checkCards();
+   
 }
 
+// start the Game
+function startGame() {
+    displayCards() 
+    checkCards();
+}
+
+
+initialGame(); 
 
 // 3. start game by clicking on the btn
 startGameBtn.addEventListener( 'click', startGame );
 drawACardBtn.addEventListener( 'click', drawACard );
+restartGameBtn.addEventListener( 'click', initialGame );
